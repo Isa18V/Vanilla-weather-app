57292964a3cb911e913d91687cd14f8be5e56ace
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-05-08 11:18:27 +0200

    deleted Fahrenheit unit in HTML ,CSS ,JS

--- src/app.js
+++ src/app.js
@@ -75,7 +75,6 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
@@ -134,43 +133,11 @@ function getLocation() {
 let locationPin = document.querySelector(`#current-button`);
 locationPin.addEventListener("click", getLocation);
 // END get geolocation
-
-// START function show F°
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
-}
-// END function show F°
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-// START function show F°
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-// END function show F°
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //START the submit or click
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchEngine);
 //END the submit or click
-
-// START F°
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-// END F°
-
-// START C°
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-// END C°
 
 search("Berlin");
 //////////////////////////////////////////////////////////////////////////////////////////////////////
